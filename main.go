cd5eff73f152aeb13bab0646654e2d17ac3334ee
Yuri Vishnevsky <yurivish@gmail.com>
2024-09-21 21:53:28 -0400

    add

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+func Must(err error) {
+	Assert(err == nil, err)
+}
+
+func Must1[T any](x T, err error) T {
+	Assert(err == nil, err)
+	return x
+}
+
+func Must2[T, U any](x T, y U, err error) (T, U) {
+	Assert(err == nil, err)
+	return x, y
+}
+
+func Assert(cond bool, msg ...any) {
+	if !cond {
+		// todo: allow Sprintf-style arguments here, with the first being a format string?
+		panic(msg)
+	}
+}
